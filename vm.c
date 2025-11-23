ad566c2baf160e32c0d2439857682dc0348f8105
Your Name <you@example.com>
2025-11-23 12:11:18 +1000

    added fetch() and eval(), which will act as the basisi of the fetch execute cycle

--- vm.c
+++ vm.c
@@ -1,7 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+typedef enum {
+    PSH,
+    ADD,
+    POP,
+    SET,
+    HLT
+} InstructionSet;
+
+const int program[] = {
+    PSH, 5,
+    PSH, 6,
+    ADD,
+    POP,
+    HLT
+};
+
+int ip = 0;
+bool running = true;
 
 int main(int argc, char** argv) {
+    while (running) {
+	int x = fetch();
+	if (x == HLT) {
+	    running = false;
+        }
+	ip++;
+    }
     return 0;
 }
 
+int fetch() {
+    return program[ip];
+}
+
+void eval(int instr) {
+    switch (instr) {
+        case HLT:
+            running = false;
+            break;
+    }
+}
